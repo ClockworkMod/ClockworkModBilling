884a481d2e30e7b67670318fa8678b42534e7696
Koushik Dutta <koushd@gmail.com>
2013-01-09 16:11:25 -0800

    remove images

--- gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/Volumes/Data/src/ClockworkModBilling/gen/com/clockworkmod/billing/R.java \
- : /Volumes/Data/src/ClockworkModBilling/res/drawable-hdpi/icon.png \
-/Volumes/Data/src/ClockworkModBilling/res/drawable-ldpi/icon.png \
-/Volumes/Data/src/ClockworkModBilling/res/drawable-mdpi/icon.png \
-/Volumes/Data/src/ClockworkModBilling/res/values/strings.xml \
-/Volumes/Data/src/ClockworkModBilling/bin/res/drawable-hdpi/icon.png \
-/Volumes/Data/src/ClockworkModBilling/bin/res/drawable-ldpi/icon.png \
-/Volumes/Data/src/ClockworkModBilling/bin/res/drawable-mdpi/icon.png \
-/Volumes/Data/src/ClockworkModBilling/bin/AndroidManifest.xml \
--- gen/com/android/vending/billing/IMarketBillingService.java.d
+++
@@ -1,3 +0,0 @@
-/Volumes/Data/src/ClockworkModBilling/gen/com/android/vending/billing/IMarketBillingService.java: \
-  /Volumes/Data/src/ClockworkModBilling/src/com/android/vending/billing/IMarketBillingService.aidl \
-
