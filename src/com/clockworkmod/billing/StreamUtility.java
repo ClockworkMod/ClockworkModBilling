50829d45495da39f9698ed0c8c7f132a011cf227
Koushik Dutta <koushd@gmail.com>
2013-01-27 18:00:28 -0800

    update

--- src/com/clockworkmod/billing/StreamUtility.java
+++ src/com/clockworkmod/billing/StreamUtility.java
@@ -9,6 +9,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -19,10 +23,37 @@ import org.json.JSONObject;
 import android.net.http.AndroidHttpClient;
 
 class StreamUtility {
-	private static final String LOGTAG = StreamUtility.class.getSimpleName();
-	public static int copyStream(InputStream input, OutputStream output) throws IOException
+    public static void fastChannelCopy(final ReadableByteChannel src, final WritableByteChannel dest) throws IOException {
+        final ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
+        while (src.read(buffer) != -1) {
+            // prepare the buffer to be drained
+            buffer.flip();
+            // write to the channel, may block
+            dest.write(buffer);
+            // If partial transfer, shift remainder down
+            // If buffer is empty, same as doing clear()
+            buffer.compact();
+        }
+        // EOF will leave buffer in fill state
+        buffer.flip();
+        // make sure the buffer is fully drained.
+        while (buffer.hasRemaining()) {
+            dest.write(buffer);
+        }
+    }
+
+	public static void copyStream(InputStream input, OutputStream output) throws IOException
 	{
-		byte[] stuff = new byte[65536];
+//	    final ReadableByteChannel inputChannel = Channels.newChannel(input);
+//	    final WritableByteChannel outputChannel = Channels.newChannel(output);
+//	    // copy the channels
+//	    fastChannelCopy(inputChannel, outputChannel);
+//	    // closing the channels
+////	    inputChannel.close();
+////	    outputChannel.close();
+
+	    
+	    byte[] stuff = new byte[65536];
 		int read = 0;
 		int total = 0;
 		while ((read = input.read(stuff)) != -1)
@@ -30,7 +61,7 @@ class StreamUtility {
 			output.write(stuff, 0, read);
 			total += read;
 		}
-		return total;
+//		return total;
 	}
     
     public static String downloadUriAsString(String uri) throws IOException {
@@ -68,7 +99,7 @@ class StreamUtility {
         {
             buff.write(stuff, 0, read);
         }
-        
+        dis.close();
         return buff.toByteArray();
     }
     
--- src/com/clockworkmod/billing/ThreadingRunnable.java
+++ src/com/clockworkmod/billing/ThreadingRunnable.java
@@ -2,26 +2,45 @@ package com.clockworkmod.billing;
 
 import android.os.Handler;
 
-public abstract class ThreadingRunnable {
-    public static void background(final ThreadingRunnable runnable) {
-        new Thread() {
+public abstract class ThreadingRunnable implements Runnable {
+    Thread thread;
+    
+    public Thread getThread() {
+        return thread;
+    }
+
+    public static ThreadingRunnable background(final ThreadingRunnable runnable) {
+        runnable.thread = new Thread() {
             public void run() {
                 runnable.run();
             };
-        }.start();
+        };
+        
+        runnable.thread.start();
+        return runnable;
+    }
+    
+    public ThreadingRunnable() {
+        mHandler = new Handler();
+    }
+
+    public ThreadingRunnable(Handler handler) {
+        mHandler = handler;
     }
 
     public abstract void run();
-    private Handler mHandler = new Handler();
+    private Handler mHandler;
     public Handler getHandler() {
         return mHandler;
     }
     
-    public void foreground(Runnable runnable) {
+    public ThreadingRunnable foreground(Runnable runnable) {
         mHandler.post(runnable);
+        return this;
     }
     
-    public void background(Runnable runnable) {
+    public ThreadingRunnable background(Runnable runnable) {
         new Thread(runnable).start();
+        return this;
     }
 }
